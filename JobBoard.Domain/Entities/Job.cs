4cab46bb776fefc1f8e93e75bf14490997ddce5c
Pooya <pooya.dev01@gmail.com>
2026-05-31 06:03:44 +0330

    introduce Company entity and job->Company relation

--- JobBoard.Domain/Entities/Job.cs
+++ JobBoard.Domain/Entities/Job.cs
@@ -8,15 +8,18 @@ namespace JobBoard.Domain.Entities
     public class Job
     {
         public Guid Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public string CompanyName { get; set; } = string.Empty;
-        public string Location { get; set; } = string.Empty;
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid CompanyId { get; set; }
+        public string Location { get; set; }
         public decimal? SalaryMin { get; set; }
         public decimal? SalaryMax { get; set; }
         public EmploymentType EmploymentType { get; set; } = EmploymentType.FullTime;
         public bool IsActive { get; set; } = true;
         public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAtUtc {  get; set; }
+
+
+        public Company Company { get; set; } = default!;
     }
 }
